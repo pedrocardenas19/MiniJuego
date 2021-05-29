ba0f4a9d645dbd6c6df278d8d11cd121b828771b
Alverth <alverth.antonio@hotmail.com>
2021-05-29 09:37:41 -0500

    Se eliminaron los print de prueba

--- PrincipalBichos.java
+++ PrincipalBichos.java
@@ -7,7 +7,7 @@ public class PrincipalBichos{
         int entrada= sc.nextInt();
         if(entrada==1){
             int numDeBichos = (int)(Math.random() * 4+1);
-            System.out.println("Se crearán "+numDeBichos+" Bichos");
+            //System.out.println("Se crearán "+numDeBichos+" Bichos");
             int contador=0;
             for(int m=0; m < numDeBichos; m++){
                 int tipoBicho = (int)(Math.random() * 2+1);
@@ -49,25 +49,26 @@ public class PrincipalBichos{
                 contador++;
                 tipoBicho = (int)(Math.random() * 2+1);
             }
-            System.out.println(bichos[0][0]);
+            /*System.out.println(bichos[0][0]);
             System.out.println(bichos[0][1]);
             System.out.println(bichos[1][0]);
-            System.out.println(bichos[1][1]); 
+            System.out.println(bichos[1][1]);*/
             int vidaTemp = 0;
             int vidaTotal = 1;
             while(true)
             {
-                if(vidaTotal == 0)
-                {
-                    System.out.println("Juego terminado, gracias por jugar (racha de asesinatos)");
-                    break;
-                }
                 System.out.println("-------------");
                 System.out.println("|"+bichos[0][0]+"|"+bichos[0][1]+"|");
                 System.out.println("-------------");
                 System.out.println("|"+bichos[1][0]+"|"+bichos[1][1]+"|");
                 System.out.println("-------------");
-
+                
+                if(vidaTotal == 0)
+                {
+                    System.out.println("Juego terminado, gracias por jugar (racha de asesinatos)");
+                    break;
+                }
+                
                 System.out.println("Elija 1 para disparar una bala \n"+
                     "Elija 2 para activar una bomba atómica \n" +
                     "Elija 3 para activar bicho mutante.");
@@ -126,30 +127,27 @@ public class PrincipalBichos{
                 }
                 else if(elegirAtaque == 3)
                 {
-                    Bichos bichoTemp;
+                    Bichos bichoTemp = null;
                     int minimo = 99;
-                    int fila = 0;
-                    int columna = 0;
                     for(int i = 0; i < bichos.length; i++){
                         for(int j = 0; j < bichos[i].length; j++){
                             if(bichos[i][j] == null)
                             {
-                                System.out.println("Ahí no hay na'");
                                 break;
+                            }
+                            else if(bichos[i][j].getSalud() == 0)
+                            {
+                                continue;
                             }
                             else if(bichos[i][j].getSalud() < minimo)
                             {
                                 bichoTemp = bichos[i][j];
                                 minimo = bichoTemp.getSalud();
-                                fila = i;
-                                System.out.println(bichos[i][j].getSalud() + "\n" + fila);
-                                columna = j;
-                                System.out.println(columna);
                             }
 
                         }
                     }
-                    bichos[fila][columna].duplicarSalud();
+                    bichoTemp.duplicarSalud();
 
                 }
                 vidaTemp = 0;
